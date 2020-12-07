9a08f3dd8816783be5899105091b50560b7af5cf
marcello <marcellocolameo@gmail.com>
2020-12-07 15:14:15 +0100

    chore: update

--- README.md
+++
@@ -1,2 +0,0 @@
-# sample_jupyter
-this is an example to explain how to version control a jupyter notebook
---
+++ example.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.6.0
+#   kernelspec:
+#     display_name: exp
+#     language: python
+#     name: exp
+# ---
+
+# %%
+print("ciao a tusstti")
+
+# %%
+print("asdasdasdasd")
+print("asdasdsaassssssssssss")
+# %%
